a81d9152cf9e93949c3e9f4207543f8b7a032054
henrique.hartwig <henrique2805@gmail.com>
2024-02-08 13:12:39 -0300

    added App component and index

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { TaskContextProvider } from './context/TaskContext';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <TaskContextProvider>
+      <App />
+    </TaskContextProvider>
+  </React.StrictMode>
+);
+
+reportWebVitals();
